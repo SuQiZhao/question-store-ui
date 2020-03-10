de6ed87d900f25c4a98ddbff320d51574455a176
sqizhao <sqizhao@linewell.com>
2020-03-10 18:41:35 +0800

    再次修改

--- src/views/tiku/index.vue
+++ src/views/tiku/index.vue
@@ -2,13 +2,15 @@
     <div class="tiku_index">
         <div class="tiku_search">
             <span style="font-family: 微软雅黑">题 库 大 全</span><br/>
-            <el-divider content-position="center" ><span style="color:#a5a5a5;font-family: 微软雅黑;font-size: 12px">EXAM CLASSIFICATION</span></el-divider>
+            <el-divider content-position="center"><span style="color:#a5a5a5;font-family: 微软雅黑;font-size: 12px">EXAM CLASSIFICATION</span>
+            </el-divider>
             <div class="tiku_searchBar">
-                <el-input  placeholder="请输入关键字搜索" style="width: 80%;margin-right: 5%"></el-input><el-button icon="el-icon-search" circle></el-button>
+                <el-input placeholder="请输入关键字搜索" style="width: 80%;margin-right: 5%"></el-input>
+                <el-button circle icon="el-icon-search"></el-button>
             </div>
         </div>
         <div class="tiku_classify">
-            <el-row >
+            <el-row>
                 <el-col :span="6">
                     <el-card class="box-card" shadow="hover">
                         <el-link :underline="false"><i class="tk2-tplist-icon1"></i></el-link>
@@ -37,20 +39,34 @@
     export default {};
 </script>
 <style lang="scss">
-    .tiku_search{
+    .tiku_search {
         text-align: center;
         margin-left: 30%;
         margin-right: 30%;
         padding-top: 5%;
         padding-bottom: 5%;
     }
-    .tiku_index{
+
+    .tiku_index {
         background-color: #ffffff;
     }
-    .tiku_classify{
+
+    .tiku_classify {
         padding: 2%;
     }
-    .tk2-tplist-icon1{
+
+    .tk2-tplist-icon1 {
+        background-image: url("../../assets/img/lzg-shouye-sasss.png");
+        background-position: -117px -95px !important;
+        height: 78px;
+        width: 82px;
+        margin-top: 40px;
+        margin-bottom: 20px;
+        display: inline-block;
+        text-align: center;
+    }
+
+    .tk2-tplist-icon2 {
         background-image: url("../../assets/img/lzg-shouye-sasss.png");
         background-position: -117px -95px !important;
         height: 78px;
@@ -59,16 +75,8 @@
         margin-bottom: 20px;
         display: inline-block;
     }
-    .tk2-tplist-icon2{
-        background-image: url("../../assets/img/lzg-shouye-sasss.png");
-        background-position: -117px -95px !important;
-        height: 78px;
-        width: 82px;
-        margin-top: 40px;
-        margin-bottom: 20px;
-        display: inline-block;
-    }
-    .box-card{
+
+    .box-card {
         text-align: center;
     }
 
